4abf38609ef811f5b3772b82ccd76b6cf6650898
Alex Ribeiro <alexprodutor.com@gmail.com>
2025-09-24 17:59:16 -0300

    Remoção de validador sem utilidade

--- src/domain/validator/VideoValidatorService.ts
+++
@@ -1,9 +0,0 @@
-import { QueueMessageMovieBody } from "../entities/QueueMessageMovieBody";
-
-export class VideoValidatorService{
-    public validate(video: QueueMessageMovieBody): boolean {
-        if(video.videoPath != null)
-            return true;
-        return false;
-    }
-}
